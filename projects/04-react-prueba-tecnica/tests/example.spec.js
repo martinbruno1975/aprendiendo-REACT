c50a33b06d1b91705244b243eb9dfb85e3581ff3
Martin Bruno <martinbruno1975@gmail.com>
2025-01-23 13:40:26 -0300

    testing N2N

--- projects/04-react-prueba-tecnica/tests/example.spec.js
+++ projects/04-react-prueba-tecnica/tests/example.spec.js
@@ -1,19 +1,17 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const LOCALHOST_URL = 'http://localhost:5173/'
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com/cat/says/"
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test('app shows random fact and image', async ({ page }) => {
+  await page.goto(LOCALHOST_URL);
+  const text = await page.getByRole('paragraph')
+  const image = await page.getByRole('img')
+  
+  const textContent = await text.textContent()
+  const src = await image.getAttribute('src')
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(textContent?.length).toBeGreaterThan(0)
+  await expect(src?.startsWith('hola')).toBeTruthy()
 });
